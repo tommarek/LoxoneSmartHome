04536ff76465a81316a3f57a893d56e086599295
Tom Marek <tm.tomas.marek@gmail.com>
2023-06-21 22:14:56 +0200

    loxone-mqtt-bridge: allow subscribing to multiple topics

--- mqtt-loxone-bridge/mqtt-loxone-bridge.py
+++ mqtt-loxone-bridge/mqtt-loxone-bridge.py
@@ -6,7 +6,7 @@ import os
 # MQTT broker details
 mqtt_host = os.getenv("MQTT_HOST", 'mosquitto')
 mqtt_port = int(os.getenv("MQTT_PORT", "1883"))
-mqtt_topic = os.getenv("MQTT_TOPIC", "energy/solar")
+mqtt_topics = os.getenv("MQTT_TOPICS", "energy/solar,teplomer/TC").split(',') # comma separated list of topics
 
 # Loxone server details
 loxone_host = os.getenv("LOXONE_HOST", "192.168.0.200")
@@ -17,18 +17,22 @@ mqtt_client = mqtt.Client()
 
 # Define the on_connect callback function for the MQTT client
 def on_connect(client, userdata, flags, rc):
-    print(f"Connected with result code {rc}")
-    client.subscribe(mqtt_topic)
+    print("Connected with result code", rc)
+    # Subscribing to multiple topics
+    for topic in mqtt_topics:
+        client.subscribe(topic)
 
 # Define the on_message callback function for the MQTT client
 def on_message(client, userdata, msg):
-    #print(f"Received message on topic {msg.topic}: {msg.payload}")
-    if msg.topic == mqtt_topic:
-        data = json.loads(msg.payload)
-        message = ';'.join([f"{k}={v}" for k, v in data.items()])
-        udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        udp.sendto(bytes(message, "utf-8"), (loxone_host, loxone_port))
-        print(f"Message sent")
+    if msg.topic in mqtt_topics:
+        try:
+            data = json.loads(msg.payload)
+            message = ';'.join([f"{k}={v}" for k, v in data.items()])
+            udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            udp.sendto(bytes(message, "utf-8"), (loxone_host, loxone_port))
+            print("Message sent")
+        except Exception as e:
+            print("Error sending message:", e)
 
 # Set the MQTT client callbacks
 mqtt_client.on_connect = on_connect
